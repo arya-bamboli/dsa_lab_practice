e150035ce730ccee2947d06b8f4ad485df0ccc80
Prathamesh Anwekar <f20201039@pilani.bits-pilani.ac.in>
2023-02-20 20:36:24 +0530

    lab1 init

---
+++ lab01/master.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "count.h"
+
+int main(void){
+    char s[100];
+    printf("enter a string: ");
+    scanf("%[^\n]s", s);
+
+    int n = count(s);
+    printf("the number of vowels in the string is %d", n);
+}
