59e04cbc0e548412bd5a559a2763c0cf3227d03b
Arya <83908779+aryad-16@users.noreply.github.com>
2023-02-24 09:21:55 +0530

    basic file io

---
+++ file_io/basic_file_io/file_read.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int num;
+    FILE *fptr;
+    fptr = fopen("program.txt", "r");
+    if (fptr == NULL)
+    {
+        printf("Error opening file");
+        // exit(1);
+    }
+    fscanf(fptr, "%d", &num);
+    printf("Value of n = %d", num);
+    fclose(fptr);
+    return 0; 
+}
