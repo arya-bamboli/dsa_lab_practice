c3cea67e89ca0b783aa2c03b9a9d4abb2d5c22f8
Arya <83908779+aryad-16@users.noreply.github.com>
2023-02-24 11:07:36 +0530

    fileIO examples added

--- file_io/hw_exercises/morse_decoding/morse_decode.c
+++ file_io/hw_exercises/morse_decoding/morse_decode.c
@@ -1,26 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
- 
-// Driver code
-int main()
-{
-    FILE* ptr;
-    char ch;
-    char str[10000];
-    ptr = fopen("msg.txt", "r");
 
-    if (NULL == ptr) {
-        printf("file can't be opened \n");
-    }
- 
-    printf("content of this file are \n");
- 
-    while (!feof(ptr)) {
-        while (fgets(str, 10000, ptr) != NULL) {
-            printf("%s ** New String **", str);
+// array for morsecode of alphabets and integers
+char *morseCode[] = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", "-----"};
+
+int getIndex(char buffer[])
+{
+    int i;
+    for (i = 0; i < 36; i++)
+    {
+        if (strcmp(buffer, morseCode[i]) == 0)
+        {
+            return i;
         }
     }
-    fclose(ptr);
-    return 0;
+    return -1;
+}
+
+char getChar(int index)
+{
+    if (index < 26)
+    {
+        return (char)(index + 65);
+    }
+    else
+    {
+        return (char)(index + 22);
+    }
+}
+
+int main()
+{
+    FILE *fptr = fopen("msg.txt", "r");
+    char ans[100];
+    int ind = 0;
+    char c;
+    char buffer[10];
+    while (fgetc(fptr) != EOF)
+    {
+        fseek(fptr, -1, SEEK_CUR);
+
+        fscanf(fptr, "%[^ ]", buffer);
+        printf("%s",buffer);
+        printf(" ");
+        if (buffer[0] == '/')
+        {
+            ans[ind++] = ' ';
+        }
+        else
+        {
+            int index = getIndex(buffer);
+            if (index != -1)
+            {
+                ans[ind++] = getChar(index);
+            }
+        }
+        fseek(fptr, 1, SEEK_CUR);
+    }
+    printf("\n %s",ans);
 }
